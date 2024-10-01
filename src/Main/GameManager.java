2119028e23de08cf6ef89e11b9011ac14c5427d1
FanchonSora <khanhngan1491@gmail.com>
2024-10-02 01:40:17 +0700

    new update

--- src/Main/GameManager.java
+++ src/Main/GameManager.java
@@ -9,6 +9,7 @@ public class GameManager extends Game{
     private static GameManager instance;
     private Game currentGame;
     GamePanel gamePanel;
+    UI ui;
     private GameManager() {}
 
     public static GameManager getInstance() {
@@ -16,6 +17,11 @@ public class GameManager extends Game{
             instance = new GameManager();
         }
         return instance;
+    }
+
+    public void setUp(GamePanel gamePanel, UI ui) {
+        this.gamePanel = gamePanel;
+        this.ui = ui;
     }
 
     public Game getCurrentGame() {
@@ -36,25 +42,35 @@ public class GameManager extends Game{
         }
     }
 
-    public void choose(String gameName) {
-        if (currentGame != null) {
-            currentGame.end();
+    public void draw() {
+        int x = 0;
+        int y = 0;
+        // DRAW MENU TITLE SCREEN:
+        ui.g2.drawImage( ui.titleScreen , x , y , null );
+        // DRAW BUTTON:
+        x += gamePanel.tileSize * 16;
+        y += gamePanel.tileSize * 5;
+        // TETRIS BUTTON:
+        if ( ui.commandNum == 0 ) {
+            ui.g2.drawImage( ui.playButton1 , x , y , null );
+        } else {
+            ui.g2.drawImage( ui.playButton2 , x , y , null );
         }
 
-        switch (gameName) {
-            case "Tetris":
-                currentGame = new Tetris(gamePanel);
-                break;
-            case "Maze":
-                currentGame = new Maze(gamePanel);
-                break;
-            default:
-                // Return if there isn't input 
-                gamePanel.gameState = gamePanel.titleState;
+        // MAZE BUTTON:
+        y += 82;
+        if ( ui.commandNum == 1 ) {
+            ui.g2.drawImage( ui.playButton1 , x , y , null );
+        } else {
+            ui.g2.drawImage( ui.playButton2 , x , y , null );
         }
-
-        // Start the selected game
-        //currentGame.start();
+        // RETURM BUTTON:
+        y += 82;
+        if ( ui.commandNum == 2 ) {
+            ui.g2.drawImage( ui.exitButton1 , x , y , null );
+        } else {
+            ui.g2.drawImage( ui.exitButton2 , x , y , null );
+        }
     }
 }
 
