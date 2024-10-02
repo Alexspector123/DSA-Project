19df413b9fe2be763dd3040ff5c05cc0f3ea672a
Phan Tran Thanh Huy <alexspector8766@gmail.com>
2024-10-02 14:52:09 +0700

    (procress)

--- src/Main/GameManager.java
+++ src/Main/GameManager.java
@@ -9,7 +9,6 @@ public class GameManager extends Game{
     private static GameManager instance;
     private Game currentGame;
     GamePanel gamePanel;
-    UI ui;
     private GameManager() {}
 
     public static GameManager getInstance() {
@@ -21,7 +20,6 @@ public class GameManager extends Game{
 
     public void setUp(GamePanel gamePanel, UI ui) {
         this.gamePanel = gamePanel;
-        this.ui = ui;
     }
 
     public Game getCurrentGame() {
@@ -43,33 +41,34 @@ public class GameManager extends Game{
     }
 
     public void draw() {
+
         int x = 0;
         int y = 0;
         // DRAW MENU TITLE SCREEN:
-        ui.g2.drawImage( ui.titleScreen , x , y , null );
+        gamePanel.ui.g2.drawImage(gamePanel.ui.titleScreen , x , y , null );
         // DRAW BUTTON:
         x += gamePanel.tileSize * 16;
         y += gamePanel.tileSize * 5;
         // TETRIS BUTTON:
-        if ( ui.commandNum == 0 ) {
-            ui.g2.drawImage( ui.playButton1 , x , y , null );
+        if (gamePanel.ui.commandNum == 0 ) {
+            gamePanel.ui.g2.drawImage(gamePanel.ui.playButton1 , x , y , null );
         } else {
-            ui.g2.drawImage( ui.playButton2 , x , y , null );
+            gamePanel.ui.g2.drawImage(gamePanel.ui.playButton2 , x , y , null );
         }
 
         // MAZE BUTTON:
         y += 82;
-        if ( ui.commandNum == 1 ) {
-            ui.g2.drawImage( ui.playButton1 , x , y , null );
+        if (gamePanel.ui.commandNum == 1 ) {
+            gamePanel.ui.g2.drawImage(gamePanel.ui.playButton1 , x , y , null );
         } else {
-            ui.g2.drawImage( ui.playButton2 , x , y , null );
+            gamePanel.ui.g2.drawImage(gamePanel.ui.playButton2 , x , y , null );
         }
         // RETURM BUTTON:
         y += 82;
-        if ( ui.commandNum == 2 ) {
-            ui.g2.drawImage( ui.exitButton1 , x , y , null );
+        if (gamePanel.ui.commandNum == 2 ) {
+            gamePanel.ui.g2.drawImage(gamePanel.ui.exitButton1 , x , y , null );
         } else {
-            ui.g2.drawImage( ui.exitButton2 , x , y , null );
+            gamePanel.ui.g2.drawImage(gamePanel.ui.exitButton2 , x , y , null );
         }
     }
 }
